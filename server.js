022282b32e6b3f6532928ef880df4b1ad2485427
JordiArnal <kanseed@gmail.com>
2016-12-21 12:11:19 +0100

    no message

--- server.js
+++ server.js
@@ -31,6 +31,26 @@ console.log("ip "+networkInterfaces);
 
 
 
+function getServerIp() {
+
+  var os = require('os');
+  var ifaces = os.networkInterfaces();
+  var values = Object.keys(ifaces).map(function(name) {
+    return ifaces[name];
+  });
+  values = [].concat.apply([], values).filter(function(val){ 
+    return val.family == 'IPv4' && val.internal == false; 
+  });
+
+  return values.length ? values[0].address : '0.0.0.0';
+}
+
+console.log("ip address "+getServerIp());
+
+console.log("end");
+
+
+
 // Routes
 app.get("/status", function (req, res) {
   res.statusCode = 200
