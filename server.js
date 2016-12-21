6dd0723c02fed76673513a2e53b6c148857da0c3
JordiArnal <kanseed@gmail.com>
2016-12-21 11:56:13 +0100

    no message

--- server.js
+++ server.js
@@ -1,33 +1,112 @@
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var cc       = require('config-multipaas'),
+    finalhandler= require('finalhandler'),
+    http     = require("http"),
+    Router       = require('router'),
+    fs = require('fs'),
+    serveStatic       = require("serve-static");
 
-var WebSocketServer = require('ws').Server
-var http = require('http');
+var config   = cc();
+var app      = Router()
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	  response.write("Welcome to Node.js on OpenShift!\n\n");
-	  response.end("Thanks for visiting us! \n");
+
+
+function ClientData(Id, Name, Socket)
+{
+    this.m_Id=Id;
+    this.m_Name=Name;
+	this.m_Socket=Socket;
+	this.m_Position="0.0, 0.0, 0.0";
+}
+
+var m_ClientsData=[];
+var io=require('socket.io')(1234);
+console.log('Kaneda Games Multiplayer Server Initialized');
+
+// Serve up public/ftp folder 
+app.use(serveStatic('static'))
+
+
+// Routes
+app.get("/status", function (req, res) {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json; charset=utf-8')
+  res.end("{status: 'ok'}\n")
+})
+
+app.get("/", function (req, res) {
+	console.log( "home");
+  var index = fs.readFileSync(__dirname + '/index.html')
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  res.end(index.toString())
+})
+
+// Create server 
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  app(req, res, done)
+})
+
+server.listen(config.get('PORT'), config.get('IP'), function () {
+  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
 });
 
-server.listen( port, ipaddress, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
+io.on('connection', function(socket) {
+	
+	var m_CurrentClient;
+	
+	m_CurrentClient=socket;
+	
+	for(i=0;i<m_ClientsData.length;++i)
+	{
+		m_ClientsData[i].m_Socket.emit('CreateClient', {
+			id : m_ClientsData.length,
+		})
+	}
+
+	var l_ClientData=new ClientData(m_ClientsData.length, '', socket)
+	m_ClientsData.push(l_ClientData);
+	
+	console.log('New client '+l_ClientData.m_Id);
+	
+	socket.on('disconnect', function() {
+		console.log('User disconnected!');
+
+		var l_ClientId=m_ClientsData.indexOf(socket);
+		m_ClientsData.splice(l_ClientId, 1);
+	});
+	
+	socket.emit('SetIdClient', {
+		id : l_ClientData.m_Id,
+	})
+	
+	
+	for(i=0;i<m_ClientsData.length;++i)
+	{
+		socket.emit('CreateClient', {
+			id : m_ClientsData[i].m_Id,
+		})
+	}
+	socket.on('ping', function (data) {
+		console.log('user ping');
+	})
+	socket.on('SetTransform', function (data) {
+		var l_Id=parseInt(data.id);
+		if(l_Id>=0 && l_Id<m_ClientsData.length)
+		{
+			m_ClientsData[l_Id].m_Position=data.position;
+			//console.log('SetTransform '+data.position);
+			for(i=0;i<m_ClientsData.length;++i)
+			{
+				if(m_ClientsData[i].m_Socket!=m_CurrentClient)
+				{
+					//console.log('send data from '+l_Id+" to "+m_ClientsData[i].m_Id);
+					m_ClientsData[i].m_Socket.emit('SetTransform', {
+						id : l_Id,
+						position : data.position,
+					})
+				}
+			}
+		}
+	})
 });
-
-wss = new WebSocketServer({
-    server: server,
-    autoAcceptConnections: false
-});
-wss.on('connection', function(ws) {
-  console.log("New connection");
-  ws.on('message', function(message) {
-    ws.send("Received: " + message);
-  });
-  ws.send('Welcome!');
-});
-
-console.log("Listening to " + ipaddress + ":" + port + "...");
-
-
-
